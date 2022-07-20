4b0c58ba3988a858a700f421a8d46583a7343c5c
arunmahara <arunmahara124@gmail.com>
2022-07-20 17:54:19 +0545

    models and migration

--- resumebuilder/admin.py
+++ resumebuilder/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'gender',
+        'title',
+        'address',
+        'email',
+        'social',
+        'number',
+        'objective',
+        'company_name',
+        'job_title',
+        'job_date_form',
+        'job_date_to',
+        'college_name',
+        'course',
+        'gpa',
+        'skills',
+        'profile_picture')
--- resumebuilder/models.py
+++ resumebuilder/models.py
@@ -1,3 +1,27 @@
+import django
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    title = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+    social = models.URLField(blank=True)
+    number = models.PositiveBigIntegerField()
+    objective = models.TextField()
+    company_name= models.CharField(max_length=100)
+    job_title = models.CharField(max_length=50)
+    job_date_form = models.DateField(auto_now=False, auto_now_add=False)
+    job_date_to = models.DateField(auto_now=False, auto_now_add=False)
+    college_name = models.CharField(max_length=100)
+    course = models.CharField(max_length=50)
+    gpa = models.DecimalField(max_digits=2, decimal_places=1)
+    skills = models.CharField(max_length=150)
+    profile_picture = models.ImageField(upload_to = 'pics', blank=True)
+
+
+
+
