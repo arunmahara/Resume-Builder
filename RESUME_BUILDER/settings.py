8a1bb5015f63bcbd17a51dc765ce79f32db187f4
arunmahara <arunmahara124@gmail.com>
2022-07-21 16:45:12 +0545

    form and template

--- RESUME_BUILDER/settings.py
+++ RESUME_BUILDER/settings.py
@@ -118,11 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'travello/static'),
-    # os.path.join(BASE_DIR, 'loginform/static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'resumebuilder/static'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- resumebuilder/views.py
+++ resumebuilder/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import ProfileForm
+from .models import Profile
 
 # Create your views here.
 def index(request):
-    # return HttpResponse ('<h1>hello</h1>')
-    return render (request, 'index.html')
+    fm = ProfileForm()
+    return render (request, 'index.html', {'form':fm})
